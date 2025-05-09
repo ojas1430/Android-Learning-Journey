be5c93b9f6f7659c4fbe05da3a41edc463c361ee
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-05-09 11:17:17 +0530

    add new line in canvas

--- app/src/main/java/com/ojasx/tp/Canvas/DrawLineScreen.kt
+++ app/src/main/java/com/ojasx/tp/Canvas/DrawLineScreen.kt
@@ -1,2 +1,25 @@
 package com.ojasx.tp.Canvas
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun DrawLineScreen() {
+    Canvas(modifier= Modifier.fillMaxSize()) {
+        val CanvasWidth = size.width
+        val CanvasHeight = size.height
+
+        drawLine(
+            color = Color.Red,
+            start = Offset(0f,CanvasHeight/2),
+            end = Offset(CanvasWidth,CanvasHeight/2),
+            strokeWidth = 10f
+        )
+    }
+}
